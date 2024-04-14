f884060fe82443d8ae14bacfdfb78d2ba01351af
moonseonghui <msh9980@naver.com>
2024-04-14 15:16:52 +0900

    FEAT : nav filter기능 추가

---
+++ ShoppingList/assets/data/itemData.js
@@ -0,0 +1,62 @@
+export const itemsData = [
+  {
+    imageUrl: "./assets/images/maru.png",
+    itemName: "마루는 강쥐",
+    itemPrice: "32000",
+    type: "books",
+  },
+  {
+    imageUrl: "./assets/images/maru.png",
+    itemName: "마루는 강쥐",
+    itemPrice: "32000",
+    type: "books",
+  },
+  {
+    imageUrl: "./assets/images/maru.png",
+    itemName: "마루는 강쥐",
+    itemPrice: "32000",
+    type: "books",
+  },
+  {
+    imageUrl: "./assets/images/maru.png",
+    itemName: "마루는 강쥐",
+    itemPrice: "32000",
+    type: "books",
+  },
+  {
+    imageUrl: "./assets/images/maru.png",
+    itemName: "마루는 강쥐",
+    itemPrice: "32000",
+    type: "books",
+  },
+  {
+    imageUrl: "./assets/images/maru.png",
+    itemName: "마루는 강쥐",
+    itemPrice: "32000",
+    type: "books",
+  },
+  {
+    imageUrl: "./assets/images/maru.png",
+    itemName: "마루는 강쥐",
+    itemPrice: "32000",
+    type: "games",
+  },
+  {
+    imageUrl: "./assets/images/maru.png",
+    itemName: "마루는 강쥐",
+    itemPrice: "32000",
+    type: "others",
+  },
+  {
+    imageUrl: "./assets/images/maru.png",
+    itemName: "마루는 강쥐",
+    itemPrice: "32000",
+    type: "others",
+  },
+  {
+    imageUrl: "./assets/images/maru.png",
+    itemName: "마루는 강쥐",
+    itemPrice: "32000",
+    type: "others",
+  },
+];
--- ShoppingList/index.js
+++ ShoppingList/index.js
@@ -1,4 +1,41 @@
+import { itemsData } from "./assets/data/itemData.js";
+
 const homeBtn = document.querySelector(".homeBtn");
 homeBtn.addEventListener("click", () => {
   window.location.href = "http://127.0.0.1:5500/ShoppingList/index.html";
 });
+
+const navLinks = document.querySelectorAll("nav ul li a");
+const boxWrapped = document.querySelector(".box_wrapped");
+
+function createItem(item) {
+  const wishBox = document.createElement("div");
+  wishBox.classList.add("wish_box");
+  wishBox.innerHTML = `
+            <img src="${item.imageUrl}" />
+            <p class="wish_btn">♥</p>
+            <p class="item_name">${item.itemName}</p>
+            <p class="item_price">${item.itemPrice}</p>
+          `;
+  return wishBox;
+}
+
+function displayItems(category) {
+  boxWrapped.innerHTML = "";
+  itemsData
+    .filter((item) => category === "all" || item.type === category)
+    .forEach((item) => {
+      boxWrapped.appendChild(createItem(item));
+    });
+}
+
+navLinks.forEach((link) => {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const category = e.target.dataset.type;
+    console.log(category);
+    displayItems(category);
+  });
+});
+
+displayItems("all");
